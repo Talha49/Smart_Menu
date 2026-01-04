749d06d9ff7b7b4835ef4d9eccae07582e37f528
Talha Ghauri <ghauritalha0@gmail.com>
2026-01-05 03:28:39 +0500

    Fix: Remove redundant basePath to fix 404 session route

--- auth.config.js
+++ auth.config.js
@@ -1,5 +1,4 @@
 export const authConfig = {
-  basePath: "/api/auth",
   pages: {
     signIn: "/login",
     error: "/login",
--- auth.js
+++ auth.js
@@ -8,7 +8,6 @@ import { authConfig } from "./auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
-  basePath: "/api/auth",
   trustHost: true,
   providers: [
     Credentials({
